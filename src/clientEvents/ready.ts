382d8bfe6a5fbdeeebf02c6d6d08b314188987b1
Leonardo Montini <lion.48m@gmail.com>
2024-03-02 16:02:48 +0100

    fix: quiz channel id

--- src/clientEvents/ready.ts
+++ src/clientEvents/ready.ts
@@ -1,8 +1,8 @@
 import { Client, TextChannel } from "discord.js";
 import { setupQuizCron } from "quiz";
-import { BOT_TEST_CHANNEL_ID } from "utils";
+import { DEV_QUIZ_CHANNEL_ID } from "../utils/constants";
 
 export const ready = (client: Client) => {
-  setupQuizCron(client.channels.cache.get(BOT_TEST_CHANNEL_ID) as TextChannel);
+  setupQuizCron(client.channels.cache.get(DEV_QUIZ_CHANNEL_ID) as TextChannel);
   console.log("Bot is ready!");
 };
