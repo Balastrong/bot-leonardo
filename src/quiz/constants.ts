4879b54b138a88b8ed2e20251b4dc8b95a3efe71
Leonardo Montini <lion.48m@gmail.com>
2024-03-06 12:39:11 +0100

    feat: change quiz scheduling

--- src/quiz/constants.ts
+++ src/quiz/constants.ts
@@ -1,5 +1,5 @@
-export const QUIZ_CRON = "0 16 * * MON,WED,FRI";
-export const ANSWER_CRON = "0 10 * * MON,WED,FRI";
+export const QUIZ_CRON = "0 14 * * MON,WED,FRI";
+export const ANSWER_CRON = "0 18 * * TUE,THU,SAT";
 
 export const QUIZ_TEST_CRON = "*/10 * * * * *";
 export const ANSWER_TEST_CRON = "15,25,35,45,55 * * * * *";
