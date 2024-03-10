57c377e973c1875ec8ddd4a1cec428504152691e
Leonardo Montini <lion.48m@gmail.com>
2024-03-10 14:28:05 +0100

    chore: set timing to two quizzes per week

--- src/quiz/constants.ts
+++ src/quiz/constants.ts
@@ -1,5 +1,5 @@
-export const QUIZ_CRON = "0 14 * * MON,WED,FRI";
-export const ANSWER_CRON = "0 18 * * TUE,THU,SAT";
+export const QUIZ_CRON = "0 14 * * MON,THU";
+export const ANSWER_CRON = "0 18 * * TUE,FRI";
 
 export const QUIZ_TEST_CRON = "*/10 * * * * *";
 export const ANSWER_TEST_CRON = "15,25,35,45,55 * * * * *";
