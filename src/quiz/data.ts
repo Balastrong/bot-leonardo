1d027a5f125f6d0484c53888bbec0df96c3d025f
Leonardo Montini <lion.48m@gmail.com>
2024-03-17 14:37:07 +0100

    feat: add 12 more quizzes

--- src/quiz/data.ts
+++ src/quiz/data.ts
@@ -111,4 +111,119 @@ export const quizzes: Quiz[] = [
     answers: ["valibot", "jest", "vitest", "node:test"],
     correctAnswerIndex: 0,
   },
+  {
+    id: "11",
+    question: "In javascript, what is the output of console.log(2 + '2' - 2);",
+    answers: ["20", "22", "02", "NaN"],
+    correctAnswerIndex: 0,
+    learnMoreUrl: "https://youtu.be/wE-6CswAE64",
+  },
+  {
+    id: "12",
+    question: "In javascript, what is the output of console.log(013 - 02);",
+    answers: ["011", "1", "9", "NaN"],
+    correctAnswerIndex: 2,
+    context:
+      "The leading 0 in a number indicates it's not in base 10 but it is an an octal number. This means 013 is 11 in base 10 and 02 is 2 in base 10 making our log in fact 11 - 2 = 9",
+    learnMoreUrl: "https://youtu.be/8gGuu9c9miY",
+  },
+  {
+    id: "13",
+    question:
+      "Challenge: How can you check if a variable is an array in JavaScript?",
+    answers: [
+      "Array.isArray(variable)",
+      "typeof variable === 'array'",
+      "variable instanceof Array",
+      "variable.isArray()",
+    ],
+    correctAnswerIndex: 0,
+  },
+  {
+    id: "14",
+    question: "What is the trigger name to manually run a GitHub Action?",
+    answers: [
+      "workflow_run",
+      "action_manual",
+      "on_demand",
+      "workflow_dispatch",
+    ],
+    correctAnswerIndex: 3,
+    learnMoreUrl: "https://youtu.be/KGfncu595pc",
+  },
+  {
+    id: "15",
+    question: "What is a GitHub Awesome List?",
+    answers: [
+      "The most active users on GitHub",
+      "A repository with a list of useful resources",
+      "A series of best practices for using GitHub",
+      "The changelog of GitHub updates",
+    ],
+    correctAnswerIndex: 1,
+    learnMoreUrl: "https://youtu.be/dqp-JFb1Upg",
+  },
+  {
+    id: "16",
+    question: "Which one is NOT a merge option for Pull Requests in GitHub?",
+    answers: ["Rebase", "Squash", "Merge", "Append"],
+    correctAnswerIndex: 3,
+    learnMoreUrl: "https://youtu.be/rFRtsiQEJZw",
+  },
+  {
+    id: "17",
+    question: "In GitHub Issues Form Schemas, which control is NOT available?",
+    answers: ["datepicker", "multiselect", "textarea", "markdown"],
+    correctAnswerIndex: 0,
+    learnMoreUrl: "https://youtu.be/hNs5Gg_fEEs",
+  },
+  {
+    id: "18",
+    question: "Which platform is NOT yet enabled for GitHub Sponsor?",
+    answers: ["Buy Me a Coffee", "Open Collective", "Patreon", "YouTube"],
+    correctAnswerIndex: 3,
+    learnMoreUrl: "https://github.com/sponsors/Balastrong",
+  },
+  {
+    id: "19",
+    question:
+      "In TanStack Router, how can you define where a component will render inside a route?",
+    answers: ["`<Render />`", "`{children}`", "`<Outlet />`", "`<slot />`"],
+    correctAnswerIndex: 2,
+    learnMoreUrl: "https://youtu.be/4sslBg8LprE",
+  },
+  {
+    id: "20",
+    question:
+      "In TanStack Router, how do you navigate to a route with a path parameter?",
+    answers: [
+      "`<Link to={'/path/' + value} />`",
+      "`<Link to='/path?id=value' />`",
+      "`<Link to='/path/$id' params={{id: value}} />`",
+      "`<Link to='/path' id={value} />`",
+    ],
+    correctAnswerIndex: 2,
+    learnMoreUrl: "https://youtu.be/xUrbLlcrIXY",
+  },
+  {
+    id: "21",
+    question:
+      "In TanStack Router, how do you access the path parameter in a component?",
+    answers: [
+      "`const { id } = useParams();`",
+      "`const id = useParam();`",
+      "`const id = useParams();`",
+      "`const { id } = useParam();`",
+    ],
+    correctAnswerIndex: 0,
+    learnMoreUrl: "https://youtu.be/xUrbLlcrIXY",
+  },
+  {
+    id: "22",
+    question:
+      "In TanStack Router, in which function do you verify if the user is authenticated?",
+    answers: ["`useEffect`", "`beforeLoad`", "`onLoad`", "`preRender`"],
+    correctAnswerIndex: 1,
+    learnMoreUrl: "https://youtu.be/O6dS0_IvvK0",
+  },
 ];
